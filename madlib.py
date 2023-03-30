7773e2804e5bd10703837b58f169daa822a96f02
santicz08 <103950044+santicz08@users.noreply.github.com>
2023-03-29 21:33:02 -0300

    Madlib
    Game from a idea froma youtube video about proyects for begginers.

---
+++ madlib.py
@@ -0,0 +1,7 @@
+famousPerson = input("who is the best player in the world: ")
+footballTeam = input("Whats the best football team in uruguay: ")
+rugbyTeam = input("Whats the best rugby team in uruguay: ")
+
+madlib = f"The best football player in the world is {famousPerson} and the best football team in Uruguay is {footballTeam}, finally the best rugby team is {rugbyTeam}"
+
+print(madlib)
